2d6827a2ba63f9898b7441953e58aa67ff7f6144
Agustín Liendo Ortiz <agusliendo2004@gmail.com>
2026-08-31 16:38:06 -0300

    Update password length validation to require at least 5 characters

--- src/modules/gestion-usuario/auth/dto/login.dto.ts
+++ src/modules/gestion-usuario/auth/dto/login.dto.ts
@@ -8,7 +8,7 @@ export class LoginDto {
 
   @IsString()
   @IsNotEmpty({ message: 'La contraseña no puede estar vacía.' })
-  @Length(8, 20, { message: 'La contraseña debe tener entre 6 y 20 caracteres.' })
+  @Length(5, 20, { message: 'La contraseña debe tener entre 6 y 20 caracteres.' })
   contrasena: string;
 
   @IsNotEmpty({ message: 'El id de la empresa no puede estar vacío.' })
